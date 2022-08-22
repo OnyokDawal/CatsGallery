6403fbba00eb1601c74f1bd3f2e9ac352e57ac46
LAPTOP-44CNUEEJ\XivCastiel <livestocker061096@gmail.com>
2022-08-22 12:27:41 +0800

    add logbox to ignore logs

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -6,14 +6,28 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import Cats from './Screen/Cats';
-
-const App = () => {
-
-  return (
-      <Cats/>
-  );
-};
-
-export default App;
+ import React from 'react';
+ import Cats from './Screen/Cats';
+ import { SafeAreaView, StatusBar, Platform,StyleSheet,LogBox } from 'react-native';
+ 
+ const App = () => {
+   LogBox.ignoreAllLogs(true);
+   return (
+     <SafeAreaView style={styles.container}>
+        <Cats/>
+     </SafeAreaView>
+     
+   );
+ };
+ 
+ export default App;
+ 
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: "#fff",
+     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+   }
+ });
+ 
